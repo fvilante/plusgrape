c6550b6e4c01467243b3ef56784b12b9fb31337f
fvilante <fvilante1@gmail.com>
2021-12-15 17:26:45 -0300

    minor change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Voce é meu amor <code>(ouviu?)</code> I love you ;D!.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is a test of static web hosting. Haha!
         </a>
       </header>
     </div>
